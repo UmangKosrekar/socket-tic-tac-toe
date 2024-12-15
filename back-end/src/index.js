fa5ec022858abfec72713e6cf3ebc2357be97dc3
w1zardOZ <67543663+w1zardOZ@users.noreply.github.com>
2024-12-15 18:00:45 +0530

    cors added

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,9 +1,19 @@
 require("dotenv").config("../.env");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const port = process.env.LOCAL_PORT || 1111;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
